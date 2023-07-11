e68316d58d6b5ddb83170bb9ccda5e1bb9f01b67
rifandaadam <47600046+rifandaadam@users.noreply.github.com>
2023-07-11 15:25:36 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,34 +5,47 @@ Date : 11 Juli 2023
 */
 document.addEventListener('contextmenu', event => event.preventDefault());
 
-const domain = 'https://rifandaadam.github.io/';
-
-function loadContent(url) {
-  $.ajax({
-    type: 'GET',
-    url: domain + url,
-    cache: false,
-    success: function(result) {
-      document.getElementById('content').innerHTML = result;
-    },
-    error: function(xhr, status, error) {
-      console.error('Failed to load content for URL:', url);
-      console.error('Error:', error);
-      document.getElementById('content').innerHTML = 'Failed to load content.';
-    }
-  });
-}
-
+var domain = "https://rifandaadam.github.io/";
 $(document).ready(function() {
-  const navigationLinks = ['home', 'project', 'about', 'skill', 'contact'];
+    function loadContent(url) {
+        $.ajax({
+            type: 'GET',
+            url: domain + url,
+            cache: false,
+            success: function(result) {
+                $("#content").html(result);
+            },
+            error: function() {
+                $("#content").html("Failed to load content.");
+            }
+        });
+    }
 
-  navigationLinks.forEach(function(link) {
-    $(`a.${link}`).click(function(event) {
-      event.preventDefault();
-      loadContent(`${link}.html`);
+    $('a.home').click(function(event) {
+        event.preventDefault();
+        loadContent("home.html");
     });
-  });
+	
+	$('a.project').click(function(event) {
+        event.preventDefault();
+        loadContent("project.html");
+    });
 
-  // Load home.html by default
-  loadContent('home.html');
+    $('a.about').click(function(event) {
+        event.preventDefault();
+        loadContent("about.html");
+    });
+
+    $('a.skill').click(function(event) {
+        event.preventDefault();
+        loadContent("skill.html");
+    });
+
+    $('a.contact').click(function(event) {
+        event.preventDefault();
+        loadContent("contact.html");
+    });
+
+    // Load home.html by default
+    loadContent("home.html");
 });
